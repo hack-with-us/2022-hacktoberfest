1ffedea67f67f57465aa3b137c565aa11dcf54d7
Kumar Shanu <80747792+Kr-Shanu@users.noreply.github.com>
2022-10-05 09:16:04 +0530

    Added hello world in java with info
    Added some very important information about the language

---
+++ scripts/hello_world.java
@@ -0,0 +1,17 @@
+// LANGUAGE: JAVA
+// AUTHOR: Kumar Shanu
+// GITHUB: https://github.com/Kr-Shanu
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+
+///////////*********.  Basic information about java *****************
+
+// Java was originally developed by James Gosling at Sun Microsystems. 
+// It was released in May 1995 as a core component of Sun Microsystems'
+// Java platform. The original and reference implementation Java compilers, 
+// virtual machines, and class libraries were originally released by Sun under proprietary licenses.
